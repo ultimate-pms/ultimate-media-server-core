a008c626fb381187d635b79c52ddf7b39c922232
David <david@nedved.com.au>
2018-07-29 13:27:54 +1000

    Add in support to trigger search for missing movies by year only

--- scripts/radarr/radarr_search-missing-movies-by-year.sh
+++ scripts/radarr/radarr_search-missing-movies-by-year.sh
@@ -9,8 +9,12 @@ RADARR_API_KEY="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 RADARR_HOST="127.0.0.1"
 RADARR_PORT="7878"
 
-CUTOFF_YEAR="1992" # Removes any 'un-downloaded' movies prior to this year.
-DELETE_FLAGS="?deleteFiles=false&addExclusion=true" # You may also add true/false flags for the query strings: deleteFiles, addExclusion
+# Year (Movie released) to match against
+SEARCH_YEAR="2018"
+
+# Optional configuration - Suggest leave as default
+RELEASE_STATUS="released" # This can one of: announced, inCinemas, released - Suggest leave as "released" to avoid fakes.
+SLEEP_PERIOD=0.5 # Suggest 0.5, 1, 5, or 10 (depending on the speed of your internet) to avoid performing too many concurrent searches against your indexers...
 
 ################################################################################################
 
@@ -19,6 +23,7 @@ jqInstalled() {
         echo 'Please install the JQ tool to: [/usr/bin/jq], before running this script. Download JQ at: https://stedolan.github.io/jq/' >&2
         exit 1
     fi
+    JQ_BIN=`which jq`
 }
 
 # Progress bar function
@@ -31,7 +36,7 @@ prog() {
 # Check JQ Installed first
 jqInstalled
 
-echo -e "++ Radarr Old Movie Remover ++\n------------------------------\n\n"
+echo -e "++ Radarr Search Missing Movies by Year ++\n-----------------------------------------\n\n"
 echo "Querying API for complete movie collection ..."
 
 TOTALITEMS=`curl -s -H "Accept: application/json" \
@@ -39,39 +44,38 @@ TOTALITEMS=`curl -s -H "Accept: application/json" \
     -H "X-Api-Key: $RADARR_API_KEY" \
     -X GET http://$RADARR_HOST:$RADARR_PORT/api/movie`
 
-i=0
-total=`echo $TOTALITEMS | jq '. | length'`
-echo -e "\nProcessing results:"
+FILTERED_RESULTS=`echo $TOTALITEMS | jq '.[] | select(.year == '$SEARCH_YEAR') | select(.status == "'$RELEASE_STATUS'") | select(.downloaded == false)'`
 
-for row in $(echo "${TOTALITEMS}" | jq -r '.[] | @base64'); do
+i=0
+total=`echo $FILTERED_RESULTS | jq '.title' | wc -l`
+
+echo -e "\nTriggering Manual Search:"
+
+
+for row in $(echo "$FILTERED_RESULTS" | jq -r '. | @base64'); do
+
     _jq() {
         echo ${row} | /usr/bin/base64 --decode | /usr/bin/jq -r ${1}
     }
+
     i=$((i + 1))
 
     MOVIENAME=`echo $(_jq '.title')`
-    MOVIEYEAR=`echo $(_jq '.year')`
-    DOWNLOADED=`echo $(_jq '.downloaded')`
     ID=`echo $(_jq '.id')`
 
     # Simple progress bar so we know how far through the script we are (great for large collections)...
     taskpercent=$((i*100/total))
     prog "$taskpercent" $MOVIENAME...
 
-    # Only remove if not downloaded yet, and older than the cutoff year
-    if [ "$DOWNLOADED" == "false" ]; then
+    # Fire off "search" against the individual movie
+    curl -s -H "Accept: application/json" \
+        -H "Content-Type: application/json" \
+        -H "X-Api-Key: $RADARR_API_KEY" \
+        --data-binary '{"name":"moviesSearch","movieIds":['$ID']}' \
+        http://$RADARR_HOST:$RADARR_PORT/api/command >> /dev/null
 
-        if [ $MOVIEYEAR -lt $CUTOFF_YEAR ]; then
-            prog "$taskpercent" ""
-            echo "FOUND: $MOVIENAME, older than year $CUTOFF_YEAR & not downloaded ... removing"
+    sleep $SLEEP_PERIOD 
 
-            curl -s -H "Accept: application/json" \
-                -H "Content-Type: application/json" \
-                --data "content=success" \
-                -H "X-Api-Key: $RADARR_API_KEY" \
-                -X DELETE http://$RADARR_HOST:$RADARR_PORT/api/movie/$ID$DELETE_FLAGS
-        fi
-    fi
 done
 
 prog "$taskpercent" ""
