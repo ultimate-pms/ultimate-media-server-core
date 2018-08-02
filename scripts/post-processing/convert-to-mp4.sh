a1642ca566307a267fcb559d5c8f230ae2bfd3d3
David <david@nedved.com.au>
2018-08-02 16:36:55 +1000

    add better support for multiple audio and subtitle streams

---
+++ scripts/post-processing/convert-to-mp4.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+# Plex works best if all your videos are in MP4 format - Most clients support "direct stream" with mp4 files...
+#
+# Here's a quick script to convert the video container to MP4 and only copy 'english' audio tracks (and remove any others)
+# No transcoding is done, so it's pretty quick and should only take 10-20 seconds per video to process.
+#
+# NB:   Metadata is also deleted "as people often embed funky metadata in the file names etc" which fixes movies
+#       from showing up funny in the plex library - This assumes that the files have already been renamed 'cleanly'
+#       using a tool like Radarr, or Filebot etc.
+#
+################################################################################################
+
+DEFAULT_LANGUAGE="eng"
+MAP_METADATA_FLAG="" # Set to "-map_metadata -1" to remove metadata or an empty string to copy default metadata over
+DELETE_SOURCE_FILES=1 # Set to 0 to disable deleting of the original media
+
+################################################################################################
+
+SOURCE_FILE="$1"
+
+fName=$(basename -- "$SOURCE_FILE")
+fExt="${fName##*.}"
+fExtLower=`echo "$fExt" | awk '{print tolower($0)}'`
+
+ffMpegInstalled() {
+    if ! [ -x "$(command -v ffmpeg)" ]; then
+        echo 'Please install ffmpeg to use this script' >&2
+        exit 1
+    fi
+}
+
+if [ "$fExtLower" == "mp4" ]; then
+    echo "No need to transform file - file is already in mp4 container."
+    exit 0
+else
+    ffMpegInstalled
+
+    # MAP IN ALL VIDEO TRACKS - Sometimes there is a static JPG DVD Cover etc...
+    VIDEO_TRACKS=`ffprobe -hide_banner -show_entries stream=index,codec_name,codec_type:stream_tags=language -of compact "$SOURCE_FILE" -v 0 | grep -i "video" | awk -F "|" '{print $2}'`
+    MAP_VIDEO=()
+    while read -r track; do
+        TRACK_NUMBER=`echo $track | tail -c 2`
+        MAP_VIDEO+=(-map 0:$TRACK_NUMBER)
+    done <<< "$VIDEO_TRACKS"
+
+    # MAP ALL AUDIO TRACKS (Sometimes there will be director commentary etc)
+    AUDIO_TRACKS=`ffprobe -hide_banner -show_entries stream=index,codec_name,codec_type:stream_tags=language -of compact "$SOURCE_FILE" -v 0 | grep -i "audio" | awk -F "|" '{print $2}'`
+
+    MAP_AUDIO=()
+    while read -r track; do
+        TRACK_NUMBER=`echo $track | tail -c 2`
+        MAP_AUDIO+=(-map 0:$TRACK_NUMBER)
+    done <<< "$AUDIO_TRACKS"
+
+    # MAP IN ALL SUBTITLE TRACKS
+    # Any subtitle formats that are not srt we won't map in ..
+    SUBTITLE_TRACKS=`ffprobe -hide_banner -show_entries stream=index,codec_name,codec_type:stream_tags=language -of compact "$SOURCE_FILE" -v 0 | grep -i "subtitle" | grep -i "srt" | awk -F "|" '{print $2}'`
+
+    MAP_SUBTITLES=()
+    while read -r track; do
+        TRACK_NUMBER=`echo $track | tail -c 2`
+        if [ ! -z "$TRACK_NUMBER" ]; then
+            # Codec for subtitle tracks with mp4 can be either: copy or mov_text
+            MAP_SUBTITLES+=(-map 0:$TRACK_NUMBER )
+        fi
+    done <<< "$SUBTITLE_TRACKS"
+    if [ ! ${#MAP_SUBTITLES[@]} -eq 0 ]; then
+        # If subtitles are mapped in, set the codec to something supported in mp4 containers.
+        MAP_SUBTITLES+=(-scodec mov_text )
+    fi
+
+    ffmpeg -loglevel fatal -hide_banner -i "$SOURCE_FILE" -vcodec copy -acodec copy $MAP_METADATA_FLAG "${MAP_VIDEO[@]}" "${MAP_AUDIO[@]}" "${MAP_SUBTITLES[@]}" -y "${SOURCE_FILE%.*}.mp4"
+    exitCode=$?
+
+    if [ $exitCode -eq 0 ]; then
+        if [ "$DELETE_SOURCE_FILES" -eq 1 ]; then
+            rm -rf "$SOURCE_FILE"
+        fi
+        exit 0
+    else
+        exit $exitCode
+    fi
+fi
---
+++ scripts/post-processing/search-nas.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# An easy way to find all existing videos that have been downloaded and run them through any post processing scripts
+# This is intended to be run just once when setting up your scripts etc...
+################################################################################################
+
+# Adjust as needed.
+SEARCH_LOCATION='/nas/*/Movies'
+LOG_FILE="pp-status.txt"
+MIN_SIZE=30M
+FILE_EXTENSIONS=(
+    "m4a"
+    "avi"
+    "m4v"
+    "mpg"
+    "mpeg"
+    "wmv"
+    "mkv"
+)
+
+# No need to edit below here.
+################################################################################################
+
+# Progress bar function
+prog() {
+    local w=50 p=$1;  shift
+    printf -v dots "%*s" "$(( $p*$w/100 ))" ""; dots=${dots// /#};
+    printf "\r\e[K|%-*s| %3d %% %s" "$w" "$dots" "$p" "$*";
+}
+
+NAME_ARGS=""
+for i in "${FILE_EXTENSIONS[@]}"; do :
+    if [[ -z "${NAME_ARGS// }" ]]; then
+        NAME_ARGS="$i"
+    else
+        NAME_ARGS="$NAME_ARGS|$i"
+    fi
+done
+
+echo -e "++ Bulk Processing ++\n--------------------------------\n"
+echo "Building file list - please be patient..."
+
+
+SEARCH_COMMAND="find $SEARCH_LOCATION -type f -size +$MIN_SIZE -regextype posix-egrep -regex \".*\\.($NAME_ARGS)\$\" -print0"
+
+process_movies=()
+while IFS=  read -r -d $'\0'; do
+    process_movies+=("$REPLY")
+done < <(eval $SEARCH_COMMAND | sort -z )
+
+count=0
+total=`echo ${#process_movies[@]}`
+echo -e "\nProcessing results:"
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+if [ "$total" -gt "100" ]; then
+    SLOW_COMMENT="(this will take a while)"
+fi
+echo "Converting $total files $SLOW_COMMENT..."
+
+for filename in "${process_movies[@]}"; do :
+
+    count=$((count + 1))
+    taskpercent=$((count*100/total))
+    shortName="${filename##*/}"
+
+    prog "$taskpercent" $shortName...
+    # ./fake-video --silent --log --video="$filename" 
+    ./convert-to-mp4.sh "$filename"
+    exitCode=$?
+
+    if [ $exitCode -eq 0 ]; then
+
+        prog "$taskpercent" ""
+
+        if [ ! -z "$LOG_FILE" ]; then
+            echo -e "OK:\t[$filename]" >> $LOG_FILE
+        fi
+    else
+        if [ ! -z "$LOG_FILE" ]; then
+            echo -e "ERROR:\t[$filename]" >> $LOG_FILE
+        fi
+    fi
+
+done
+
+echo ""
